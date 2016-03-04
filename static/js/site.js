8945c7627c33b618ed43e5b31d91e722dfa3aeaa
Zach Klippenstein <zach.klippenstein@gmail.com>
2016-03-04 17:01:48 -0600

    Rewriting with flexbox and no frameworks.
    Legacy browsers are no longer supported.

---
+++ static/js/site.js
@@ -0,0 +1,72 @@
+"use strict";
+
+var navLinks = document.querySelectorAll("li > a");
+
+function parseFragment(url) {
+    if (!url) {
+        return null;
+    }
+
+    let i = url.lastIndexOf("#");
+    if (i < 0) {
+        return null;
+    }
+
+    return url.substring(i + 1);
+}
+
+function showPage(id) {
+    id = parseFragment(id);
+    if (!id) {
+        throw "no page ID";
+    }
+
+    let nodeList = document.querySelectorAll("li > a");
+    for (let i = 0; i < nodeList.length; i++) {
+        let link = nodeList.item(i);
+        let linkFragment = parseFragment(link.href);
+        link.classList.toggle("selected", linkFragment === id);
+    }
+
+    nodeList = document.querySelectorAll(".page");
+    var selectedPage = null;
+    for (let i = 0; i < nodeList.length; i++) {
+        let page = nodeList.item(i);
+        if (page.id === id) {
+            selectedPage = page;
+            page.classList.toggle("hidden-when-wide", false);
+        } else {
+            page.classList.toggle("hidden-when-wide", true);
+        }
+    }
+
+    if (!selectedPage) {
+        // No page found with id.
+        showPage(navLinks[0].href);
+    } else if (!(id === parseFragment(window.location.hash))) {
+        // A new page was selected, update the address bar.
+        window.location.hash = id;
+    }
+
+}
+
+function init() {
+    window.addEventListener("hashchange", function (e) {
+        showPage(window.location.hash);
+    });
+
+    if (window.location.hash) {
+        showPage(window.location.hash);
+    } else if (navLinks.length > 0) {
+        let firstPage = navLinks[0].href;
+
+        // Hide pages until the "redirect" takes effect.
+        showPage(firstPage);
+
+        // Select the first navlink by default.
+        window.location.href = firstPage;
+    }
+}
+init();
+
+
